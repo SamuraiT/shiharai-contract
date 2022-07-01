596fd76f224b5236fc9d669f3446b3db6e0aa774
SamuraiT <t.yasukawa01@gmail.com>
2022-07-01 15:02:13 +0900

    feat: Add deployment

--- scripts/mint.ts
+++ scripts/mint.ts
@@ -1,22 +1,20 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import { ethers } from 'hardhat'
+import 'dotenv/config'
 
 const main = async () => {
   const Usdc = await ethers.getContractFactory('MockERC20')
-  const usdc = await Usdc.deploy('test-usdc')
-  await usdc.deployed()
+  const usdc = await Usdc.attach('')
 
   console.log('usdc', usdc.address)
-  const [owner] = await ethers.getSigners()
-  const Shihari = await ethers.getContractFactory('Shiharai')
-  const shihari = await Shihari.deploy(usdc.address)
-  await shihari.deployed()
-  await usdc.mint(owner.address, ethers.utils.parseUnits('500000000000000', 18))
-  console.log('shihari deployed to:', shihari.address)
+  const accounts = JSON.parse(process.env.ACCOUNTS || '')
+  const amount = ethers.utils.parseUnits('5000000', 18)
+  for (const account of accounts) {
+    usdc.mint(account, amount)
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
